94ce4409240827af0ea4c8480f48d4c2f71a258a
chrisvanlier2005 <87958@glr.nl>
2022-10-12 20:28:51 +0200

    - code cleanup + back button cursor - text.css updated to have title

--- resources/js/Pages/Projects/ShowProject.vue
+++ resources/js/Pages/Projects/ShowProject.vue
@@ -23,31 +23,19 @@ const user = computed(() => usePage().props.value.auth.user);
 <template>
 <Layout>
     <div class="flex min-h-[50vh] relative gap-6  items-center justify-center">
-        <div class="absolute left-0 top-12 w-fit h-fit  z-[99]">
+        <div class="absolute left-0 top-12 w-fit h-fit  z-[99] cursor-pointer   ">
             <IconX @click="back()"></IconX>
         </div>
         <halftoneBackground></halftoneBackground>
-
         <faded-circle style="right: 0; top: 0; background: linear-gradient(#ff0000, #ffab00); width: 40rem; height: 40rem; filter: blur(300px)"></faded-circle>
         <faded-circle style="left: 0; bottom: 0; background: linear-gradient(#0077ff, #d000ff); width: 40rem; height: 40rem; filter: blur(300px)"></faded-circle>
-        <!--            <GridCardSmall v-for="project in projects" :project="project"></GridCardSmall>-->
         <div class="z-[99] relative">
             <h1 class="text-6xl font-semibold filter-shadow text-center">{{project.title}}</h1>
             <p class="text-center">{{project.description}}</p>
-
         </div>
     </div>
     <section class="relative min-h-screen flex flex-col z-[99] gap-3">
         <Link v-if="user" :href="'/projects/' + project.id + '/edit'" class="bg-blue-500 px-2 z-[99] rounded-md w-fit">Edit</Link>
-
-
-        <!--        <div class="pb-4">
-                    <Link v-if="user" :href="'/projects/' + project.id + '/edit'">Edit</Link>
-                    <h1 class="text-6xl font-bold">{{project.title}}</h1>
-                    <p>{{project.description}}</p>
-
-                </div>-->
-
         <div class="prose bg-white rounded-md max-w-none w-full px-4 py-2" v-html="project.html"></div>
         <faded-circle style="right: 0; top: -20%;background: linear-gradient(red, orange);width: 40rem; height: 40rem;filter: blur(300px); z-index: -1;"></faded-circle>
     </section>
